11f37f10ca966522b058380c8ee4af74dd962d06
Alana Correia <alanacorreia.ac@gmail.com>
2022-07-16 20:36:41 -0300

    requisitos 2, 4, 6, 8, 10

--- app/backend/src/middlewares/errorMiddleware.ts
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -2,7 +2,6 @@ import { ErrorRequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
-  console.log(err);
   if (err.status) return res.status(err.status).json({ message: err.message });
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: err.message });
 };
---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,88 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/user';
+
+import { Response } from 'superagent';
+import { StatusCodes } from 'http-status-codes';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Object User', () => {
+  const mockUser = {
+    id: 1,
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+  }
+
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(mockUser as User);
+  });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('O método post /login retorna o status 200 e um token caso contenha os dados corretos', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admim.com', password: 'secret_admin' });
+
+    expect(chaiHttpResponse).to.have.status(StatusCodes.OK);
+    expect(chaiHttpResponse.body).to.have.property('token');
+  });
+
+  it('O método post /login retorna o status 400 e a mensagem "All fields must be filled", caso tente fazer o login sem email', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ password: 'secret_admin' });
+
+    expect(chaiHttpResponse).to.have.status(StatusCodes.BAD_REQUEST);
+    expect(chaiHttpResponse.body).to.have.property('message');
+    expect(chaiHttpResponse.body.message).to.have.equal('All fields must be filled');
+  });
+  
+  it('O método post /login retorna o status 400 e a mensagem "All fields must be filled", caso tente fazer o login sem informar uma senha', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admim.com' });
+
+    expect(chaiHttpResponse).to.have.status(StatusCodes.BAD_REQUEST);
+    expect(chaiHttpResponse.body).to.have.property('message');
+    expect(chaiHttpResponse.body.message).to.have.equal('All fields must be filled');
+  });
+  
+  it('O método post /login retorna o status 401 e a mensagem "Incorrect email or password", caso tente fazer o login com senha inválida', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admim.com', password: 'secret_123' });
+
+    expect(chaiHttpResponse).to.have.status(StatusCodes.UNAUTHORIZED);
+    expect(chaiHttpResponse.body.message).to.have.equal('Incorrect email or password');
+  });
+  
+  it('O método post /login retorna o status 401 e a mensagem "Incorrect email or password", caso tente fazer o login com email inválido', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@user.com', password: 'secret_123' });
+
+    expect(chaiHttpResponse).to.have.status(StatusCodes.UNAUTHORIZED);
+    expect(chaiHttpResponse.body.message).to.have.equal('Incorrect email or password');
+  });
+});
